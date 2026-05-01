e0445fe1b37f03d130806a58d6ac612b0dfe660d
Manthan Sumbhe <113107477+Bucky789@users.noreply.github.com>
2026-05-01 03:55:48 -0400

    Update route.ts

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -4,7 +4,7 @@ export async function POST(req: Request) {
   try {
     const body = await req.json();
 
-    const res = await fetch("https://lexmark-iowa-legs-finance.trycloudflare.com/chat", {
+    const res = await fetch("https://teachers-licenses-pre-geek.trycloudflare.com/chat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
